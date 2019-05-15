52c59f2ece3ae0339666f6f8bf37d687b242b5e4
Jacob McBlane <jacobmcblane@gmail.com>
2019-05-15 19:18:15 -0400

    Remove fold. Remove tput. Clean code.

--- nimph.nim
+++ nimph.nim
@@ -1,12 +1,8 @@
 # Author: Jacob McBlane
 # gopher.silentmessengers.org
-# Version: 0.0.4
+# Version: 0.0.45
 #
-# Depends upon:
-#           * fold
-# These should be already installed on any linux system.
-# Customize the variables on lines ~33 to fit your needs.
-# Customize lines ~115 for formatting output
+# Customize the variables on lines ~22 to fit your needs.
 #
 # Report any bugs to me. Contact info at my gopherhole.
 # Note: There are surely bugs!
@@ -17,17 +13,11 @@
 #       - Would like the image program to be asynchronous.
 #       - Check for uri type when loading initial uri. (Partially in)
 #       - Open current directory in pager
-#       - Remove fold dependency (striving toward pure nim)
+#       - Dynamic ports? See proc dl_uri
 
 import os, osproc, re, browsers
 import md5, parseutils, strutils
-import net, sequtils
-
-var
- tour = newSeq[string]()
- nav = newSeq[string]()
- history = newSeq[string]()
- t_height: int
+import net, sequtils, terminal
 
 let
  home = "gopher.silentmessengers.org"
@@ -35,7 +25,34 @@ let
  bookmarks = getHomeDir() & ".config/nimphmarks"
  pager = "$PAGER"
  img_app = "sxiv -a"
- fold_width = "65"
+ fold_width = 65
+ www = " 爵 "
+ txt = "   "
+ dir = "   "
+ err = "   "
+ fts = "   "
+ tel = "   "
+ bin = "   "
+ img = "   "
+ errorpage = @[@["", "", "", "", ""],
+            @["i", "", "", "Err", ""],
+            @["i", "", "", "Err", ""],
+            @["i", "Error:", "", "Err", ""],
+            @["i", "------", "", "Err", ""],
+            @["i", "", "", "Err", ""],
+            @["i", "Couldn't navigate to page.", "", "Err", ""],
+            @["i", "", "", "Err", ""],
+            @["i", "Looks like the request timed out or something.", "", "Err", ""],
+            @["i", "", "", "Err", ""],
+            @["1", "    Visit gopher.silentmessengers.org", "/", "gopher.silentmessengers.org", "70"],
+            @["7", "    Search w/ Veronica II", "/v2/vs", "gopher.floodgap.com", "70"],
+            @["i", "", "", "Err", ""],
+            @["i", "", "", "Err", ""]]
+
+var
+ tour = newSeq[string]()
+ nav = newSeq[string]()
+ history = newSeq[string]()
 
 if dirExists(tmpdir) == false:
   createDir(tmpdir)
@@ -51,15 +68,16 @@ proc type_uri(uri: string): char =
   elif uri.match(re"(.*)/g/(.*)"): return 'g'
   else: return '1'
 
-proc dl_uri(uri: string, filename: string): string =
+proc dl_uri(uri: string, filename: string, port = 70): string =
   let split = uri.split("/")
   let file = tmpdir & filename
   var socket = newSocket()
-  socket.connect(split[0], Port(70)) # Varying ports necessary?
+  try: socket.connect(split[0], Port(port)) # Varying ports necessary?
+  except: return "nothing"
   case type_uri(uri):
     of '0', '1', '7':
       if split.len() > 2:
-        socket.send("/" & split[2..high(split)].join("/") & "\r\L")
+        socket.send("/" & split[2..^1].join("/") & "\r\L")
       else:
         socket.send("/\r\L")
       var get_lines: seq[string]
@@ -67,26 +85,22 @@ proc dl_uri(uri: string, filename: string): string =
         history.add(uri)
         try:
           while true:
-            let line = socket.recvLine(timeout = 10000)
-            if line != "":
-              get_lines.add(line)
-            else:
-              break
+            let line = socket.recvLine(timeout = 30000)
+            if line != "": get_lines.add(line)
+            else: break
           writeFile(file, get_lines.join("\n"))
-        except:
-          return "nothing"
+        except: return "nothing"
       return file
     of 'I', 'g', '9':
       if fileExists(file) == false:
-        socket.send("/" & split[2..high(split)].join("/") & "\r\L")
-        file.writeFile(socket.recv(size = 10000000, timeout = 10000))
+        socket.send("/" & split[2..^1].join("/") & "\r\L")
+        file.writeFile(socket.recv(size = 10000000, timeout = 30000))
           # Ugh, I don't like having to specify the size... but I
           # don't know a way around this. I just threw in something
           # arbitrary that should be high enough to receive most things.
           # Let me know if you run into any trouble.
       return file
-    else:
-      return "nothing"
+    else: return "nothing"
 
 proc cache_uri(uri: string): string =
   let md5 = getMD5(uri)
@@ -97,22 +111,9 @@ proc get_dir(uri: string): seq[seq[string]] =
   let dir = cache_uri(uri)
   if fileExists(dir) == false:
     discard nav.pop()
-    return @[@["", "", "", "", ""],
-            @["i", "", "", "Err", ""],
-            @["i", "", "", "Err", ""],
-            @["i", "Error:", "", "Err", ""],
-            @["i", "------", "", "Err", ""],
-            @["i", "", "", "Err", ""],
-            @["i", "Couldn't navigate to " & uri, "", "Err", ""],
-            @["i", "", "", "Err", ""],
-            @["i", "Looks like the request timed out or something.", "", "Err", ""],
-            @["i", "", "", "Err", ""],
-            @["1", "    Visit gopher.silentmessengers.org", "/", "gopher.silentmessengers.org", "70"],
-            @["7", "    Search w/ Veronica II", "/v2/vs", "gopher.floodgap.com", "70"],
-            @["i", "", "", "Err", ""],
-            @["i", "", "", "Err", ""]]
+    return errorpage
   else:
-    let ls = readFile(dir).split("\n")
+    let ls = readFile(dir).splitLines()
     var dira = newSeq[seq[string]]()
     dira.add(@["i", "", "", "(NULL)", ""])
     var c = 1
@@ -121,7 +122,7 @@ proc get_dir(uri: string): seq[seq[string]] =
       let cols = i.split("\t")
       if cols.len() > 3:
         lsa.add($cols[0][0])
-        lsa.add(cols[0][1..high(cols[0])])
+        lsa.add(cols[0][1..^1])
         lsa.add(cols[1])
         lsa.add(cols[2])
         lsa.add(cols[3])
@@ -137,30 +138,24 @@ proc print_dir(dira: seq[seq[string]], info: bool, uri: string) =
     len_c += 1
   stdout.write("\n\n\n")
   var c = 1
-  for i in dira[1..high(dira)]:
+  for i in dira[1..^1]:
     case i[0]
     of "i":
       if info == true: echo "      ", i[1]
-    of "h": echo "\e[1;1m", c, "\e[1;32m", " 爵 ", i[1], "\e[0m"
-    of "0": echo "\e[1;1m", c, "\e[1;37m", "   ", i[1], "\e[0m"
-    of "1": echo "\e[1;1m", c, "\e[1;34m", "   ", i[1], "\e[0m"
-    of "3": echo "\e[1;1m", c, "\e[1;31m", "   ", i[1], "\e[0m"
-    of "7": echo "\e[1;1m", c, "\e[1;33m", "   ", i[1], "\e[0m"
-    of "8": echo "\e[1;1m", c, "\e[1;35m", "   ", i[1], "\e[0m"
-    of "9": echo "\e[1;1m", c, "\e[1;31m", "   ", i[1], "\e[0m"
-    of "I": echo "\e[1;1m", c, "\e[1;36m", "   ", i[1], "\e[0m"
-    of "g": echo "\e[1;1m", c, "\e[1;36m", "   ", i[1], "\e[0m"
+    of "h": echo "\e[1;1m", c, "\e[1;32m", www, i[1], "\e[0m"
+    of "0": echo "\e[1;1m", c, "\e[1;37m", txt, i[1], "\e[0m"
+    of "1": echo "\e[1;1m", c, "\e[1;34m", dir, i[1], "\e[0m"
+    of "3": echo "\e[1;1m", c, "\e[1;31m", err, i[1], "\e[0m"
+    of "7": echo "\e[1;1m", c, "\e[1;33m", fts, i[1], "\e[0m"
+    of "8": echo "\e[1;1m", c, "\e[1;35m", tel, i[1], "\e[0m"
+    of "9": echo "\e[1;1m", c, "\e[1;31m", bin, i[1], "\e[0m"
+    of "I": echo "\e[1;1m", c, "\e[1;36m", img, i[1], "\e[0m"
+    of "g": echo "\e[1;1m", c, "\e[1;36m", img, i[1], "\e[0m"
     else: echo "  ??  ", i[1]
     c += 1
 
-proc get_image(uri: string): int =
-  let x = cache_uri(uri)
-  return execShellCmd(img_app & " " & x)
-
 proc do_file(dira: seq[seq[string]], num: string): string =
-  var no: int
-  discard parseInt(num, no)
-  let line = dira[no]
+  let line = dira[num.parseInt()]
   let link = line[3] & "/" & line[0] & line[2]
   let file = cache_uri(link)
   return file
@@ -170,8 +165,8 @@ proc main_loop(uri: string) =
   case type_uri(uri):
   of '0':
     let file = cache_uri(paramStr(1))
-    let text = readFile(file)
-    echo readFile(file)
+    if file != "Nothing": echo readFile(file)
+    else: print_dir(errorpage, true, uri)
   else: discard
 
   let dira = get_dir(uri)
@@ -186,118 +181,94 @@ proc main_loop(uri: string) =
       let newuri = dira[y][3] & "/" & dira[y][0] & dira[y][2]
       case dira[y][0]
       of "0":
-        discard parseInt(execProcess("tput lines"), t_height)
         let file = cache_uri(newuri)
-        let text = readFile(file)
-        if text.split("\n").len() > t_height:
-          discard execShellCmd(pager & " " & file)
-        else: echo text
-      of "1":
-        main_loop(newuri)
+        if file != "Nothing":
+          let text = readFile(file)
+          if text.split("\n").len() > terminalHeight():
+            discard execShellCmd(pager & " " & file)
+          else: echo text
+        else: print_dir(errorpage, true, uri)
+      of "1": main_loop(newuri)
       of "7":
         stdout.write("Enter your query: ")
         let q = readLine(stdin)
-        let path = dira[y][2].split("?")
-        let link = dira[y][3] & "/7" & path[0] & "?" & q
+        let path = dira[y][2].split("?")[0]
+        let link = dira[y][3] & "/7" & path & "\t" & q
         main_loop(link)
       of "8":
         let path = dira[y][3] & " " & dira[y][4]
-        let a = execShellCmd("telnet " & path)
-        if a == 1: echo path
+        # discard execShellCmd("telnet " & path) # Alternative
+        echo "Telnet: ", path
       of "9":
         stdout.write("Enter a filename: ")
         let f = readLine(stdin)
-        let file = dl_uri(newuri, f)
-        echo "Downloaded to ", file
-      of "h":
-        let link = dira[y][2][4..high(dira[y][2])]
-        openDefaultBrowser(link)
-      of "I", "g":
-        discard get_image(newuri)
+        echo "Downloaded to ", dl_uri(newuri, f)
+      of "h": openDefaultBrowser(dira[y][2][4..^1])
+      of "I", "g": discard execShellCmd(img_app & " " & cache_uri(newuri))
     else:
       case x[0]
       of "go":
         if x.len() > 1:
-          if x[1].match(re"(.*)/0/(.*)"):
-            let file = cache_uri(x[1])
-            let text = readFile(file)
-            echo readFile(file)
+          if x[1].match(re"(.*)/0/(.*)"): echo readFile(cache_uri(x[1]))
           else: main_loop(x[1])
       of "ls": print_dir(dira, false, uri)
       of "cat":
-        if x.len() > 1:
-          let file = do_file(dira, x[1])
-          let text = readFile(file)
-          echo readFile(file)
+        if x.len() > 1: echo readFile(do_file(dira, x[1]))
         else: print_dir(dira, true, uri)
       of "fold":
         if x.len() > 1:
           let file = do_file(dira, x[1])
-          let text = execShellCmd("fold -w " & fold_width & " -s " & file)
+          echo wordWrap(readFile(file), fold_width, false)
       of "less":
         # Make this able to open the current dir in less
         if x.len() > 1:
-          let file = do_file(dira, x[1])
-          let i = execShellCmd(pager & " " & file)
+          discard execShellCmd(pager & " " & do_file(dira, x[1]))
       of "b", "back":
         if nav.len() > 1:
-          let current = nav.pop()
+          discard nav.pop()
           main_loop(nav.pop())
         else: main_loop(home)
       of "h", "hist", "history":
-        for i in history:
-          echo i
-      of "home":
-        main_loop(home)
+        for i in history: echo i
+      of "home": main_loop(home)
       of "search":
         stdout.write("Enter a search query: ")
         let q = readLine(stdin)
-        let link = "gopher.floodgap.com/7/v2/vs?" & q
-        main_loop(link)
+        main_loop("gopher.floodgap.com/7/v2/vs?" & q)
       of "url":
         if x.len() > 1:
-          var no: int
-          var link: string
-          discard parseInt(x[1], no)
+          let no = x[1].parseInt()
           if no < dira.len() and no > 0:
+            var link: string
             let line = dira[no]
             case line[0]
-            of "h":
-              link = line[2][4..high(line[2])]
-            of "0", "1", "7", "9", "I", "g":
-              link = line[3] & "/" & line[0] & line[2]
-            of "8":
-              link = line[3] & " " & line[4]
+            of "h": link = line[2][4..^1]
+            of "8": link = line[3] & " " & line[4]
+            else: link = line[3] & "/" & line[0] & line[2]
             echo link
         else: echo uri
       of "up":
-        var a: int
         let x = uri.split("/")
-        if x.len() > 1:
-          a = x.len() - 2
-          main_loop(x[0..a].join("/"))
+        if x.len() > 1: main_loop(x[0..^2].join("/"))
       of "tour":
         if x.len() > 1:
           if x[1].match(re"(.*)-(.*)"):
-            let t_range = x[1].split("-")
-            var st: int
-            var en: int
-            discard parseInt(t_range[0], st)
-            discard parseInt(t_range[1], en)
+            let
+              t_range = x[1].split("-")
+              st = t_range[0].parseInt()
+              en = t_range[1].parseInt()
             if st < dira.len() and en < dira.len() and st > 0 and en > 0:
               for i in st..en:
                 let line = dira[i]
                 tour.add(line[3] & "/" & line[0] & line[2])
           else:
-            for i in x[1..high(x)]:
-              var no: int
-              discard parseInt(i, no)
+            for i in x[1..^1]:
+              let no = i.parseInt()
               if no < dira.len() and no > 0:
                 let line = dira[no]
                 tour.add(line[3] & "/" & line[0] & line[2])
         else:
-          for i in tour:
-            echo i
+          for i in tour: echo i
       of "n", "next":
         if tour.len() > 0:
           let next = tour[0]
@@ -318,9 +289,7 @@ proc main_loop(uri: string) =
             f.writeLine(newuri & " :: " & desc)
         else: f.writeLine(uri & "  :: " & desc)
       of "marks", "bookmarks":
-        if fileExists(bookmarks):
-          let x = readFile(bookmarks)
-          echo x
+        if fileExists(bookmarks): echo readFile(bookmarks)
         else: echo "No bookmarks."
       of "dl", "download":
         if x.len() > 1:
@@ -330,8 +299,10 @@ proc main_loop(uri: string) =
             let newuri = dira[y][3] & "/" & dira[y][0] & dira[y][2]
             stdout.write("Enter a filename: ")
             let name = readLine(stdin)
-            let f = dl_uri(newuri, name)
-            echo "Saved to " & f
+            echo "Saved to " & dl_uri(newuri, name)
+      of "clean":
+        removeDir(tmpdir)
+        createDir(tmpdir)
       of "q", "quit", "exit":
         removeDir(tmpdir)
         quit(QuitSuccess)
@@ -403,6 +374,10 @@ proc main_loop(uri: string) =
             echo "\e[1;31m", "marks, bookmarks"
             echo "\e[1;37m", "Display a list of your bookmarks."
             stdout.write("\e[0m")
+          of "clean":
+            echo "\e[1;31m", "clean"
+            echo "\e[1;37m", "Clears the cache directory."
+            stdout.write("\e[0m")
           of "quit":
             echo "\e[1;31m", "q, quit, exit"
             echo "\e[1;37m", "Exit the application."
@@ -411,8 +386,8 @@ proc main_loop(uri: string) =
         else:
           echo "\e[1;33m", "Type a number to navigate to that uri.\n"
           echo "\e[1;31m", "Quick reference:"
-          echo "\e[1;37m", "go, ls, cat, fold, less, back, history, home"
-          echo "\e[1;37m", "add, bookmarks, search, url, up, tour, next, quit\n"
+          echo "\e[1;37m", "go, ls, cat, fold, less, back, history, home, add"
+          echo "\e[1;37m", "bookmarks, search, url, up, tour, next, clean, quit\n"
           echo "\e[1;36m", "Type help (command) for more information.", "\e[0m"
 
 if paramCount() > 0: main_loop(paramStr(1))
